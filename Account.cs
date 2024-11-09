6b6ee3ef3875a951d814aac3cd170bdb096c16de
M1nhGG69 <hlminhktpm2311023@student.ctuet.edu.vn>
2024-11-09 14:00:04 +0700

    bai1demo

---
+++ Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1demo
+{
+    internal class Account
+    {
+        private string stk;
+        private string money;
+
+        public Account(string stk, string money)
+        {
+             this.Stk = stk;
+            this.Money = money;
+        }
+
+        public string Stk { get => stk; set => stk = value; }
+        public string Money { get => money; set => money = value; }
+
+        public override string ToString()
+        {
+            return $"Mã số tài khoản: {stk} \nSố tiền: {money}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account("123456","10000");
+            Console.WriteLine(account);
+            Console.WriteLine("Thong tin tai khoan sau khi cap nhat");
+            account.Money = "800000";
+            Console.WriteLine(account);
+            Console.ReadLine(); 
+        }
+    }
+}
